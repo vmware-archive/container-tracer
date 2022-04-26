c5b337de455678666f789c41a6dceafbeb12ccf9
Enyinna Ochulor <eochulor@vmware.com>
2022-04-26 11:47:25 -0500

    Adding the API server directory
    Signed-off-by: Enyinna Ochulor <eochulor@vmware.com>

---
+++ server/swagger_server/controllers/authorization_controller.py
@@ -0,0 +1,9 @@
+from typing import List
+"""
+controller generated to handled auth operation described at:
+https://connexion.readthedocs.io/en/latest/security.html
+"""
+def check_BasicAuth(username, password, required_scopes):
+    return {'test_key': 'test_value'}
+
+
---
+++ server/swagger_server/controllers/default_controller.py
@@ -0,0 +1,48 @@
+import connexion
+import six
+
+from swagger_server import util
+
+
+def system_call_delete():  # noqa: E501
+    """system_call_delete
+
+    Remove the specified tracing resource # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def system_call_get():  # noqa: E501
+    """system_call_get
+
+    Returns a tracing result object of a system call trace of an event # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def system_call_post():  # noqa: E501
+    """system_call_post
+
+    Creates a tracing resource # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
+
+
+def system_call_put():  # noqa: E501
+    """system_call_put
+
+    Update or create the specified tracing resource # noqa: E501
+
+
+    :rtype: None
+    """
+    return 'do some magic!'
