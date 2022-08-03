9d5d91a36e27323ee0efea6aafe791901c2683df
Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>
2022-08-03 05:35:13 +0300

    Add version to the API
    Using API versioning is a good practice and very useful in API upgrades
    to keep compatibility.
    
    Signed-off-by: Tzvetomir Stoyanov (VMware) <tz.stoyanov@gmail.com>

--- server/api/routers.go
+++ server/api/routers.go
@@ -5,14 +5,18 @@ import (
 	ctx "gitlab.eng.vmware.com/opensource/tracecruncher-api/internal/tracerctx"
 )
 
+var (
+	apiVersion = "v1"
+)
+
 // map request path to logic
 func NewRouter(t *ctx.Tracer) *gin.Engine {
 	router := gin.Default()
-	router.GET("/traces", t.SystemCallGet)
-	router.GET("/traces/:id", t.SystemCallStatus)
-	router.POST("/traces", t.SystemCallPost)
-	router.DELETE("/traces/:id", t.SystemCallDelete)
-	router.GET("/pods", t.LocalPodsGet)
-	router.GET("/trace-hooks", t.TraceHooksGet)
+	router.GET("/"+apiVersion+"/traces", t.SystemCallGet)
+	router.GET("/"+apiVersion+"/traces/:id", t.SystemCallStatus)
+	router.POST("/"+apiVersion+"/traces", t.SystemCallPost)
+	router.DELETE("/"+apiVersion+"/traces/:id", t.SystemCallDelete)
+	router.GET("/"+apiVersion+"/pods", t.LocalPodsGet)
+	router.GET("/"+apiVersion+"/trace-hooks", t.TraceHooksGet)
 	return router
 }
